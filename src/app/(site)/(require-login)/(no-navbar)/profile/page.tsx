c2ef83b08529bfbf82ae71f207b6b364968438f8
farhanfahreezy <mohdfarhanfahrezy@gmail.com>
2024-07-13 00:48:20 +0700

    fix: session on profile

--- src/app/(site)/(require-login)/(no-navbar)/profile/page.tsx
+++ src/app/(site)/(require-login)/(no-navbar)/profile/page.tsx
@@ -1,40 +1,43 @@
 "use client";
 
-import React from 'react';
-import { FaArrowLeft } from 'react-icons/fa';
+import React from "react";
+import { FaArrowLeft } from "react-icons/fa";
 import LowerProps from "./comp/lower";
 import UpperProps from "./comp/upper";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-    return (
-        <div className="flex flex-col items-center justify-start h-full w-full">
-            <div className="flex flex-col w-full">
-                <div className="flex items-center w-full py-3 shadow-md">
-                    <a href='/settings'>
-                     <FaArrowLeft className="ml-8"/>
-                    </a>
-                    <div className="text-[24px] font-rubik font-medium w-full text-center mr-12">
-                        Profile
-                    </div>
-                </div>
+  const session = useSession();
 
-                <UpperProps
-                    urlImage="/expert/homelander.jpg"
-                    name="Fahrezy"
-                    role="Advocate Contributor"
-                    reputationScore="2235"
-                />
-                <h2 className="mt-4 mx-10 text-center">
-                    With just 27 more Upvotes you can become an Influencer Contributor
-                </h2>
-
-                <hr className="border-t border-gray-300 mx-10 mt-7" />
-
-                <LowerProps
-                    fullName="Ahmad Fahrezy"
-                    email="KAKGEMPAHAM@email.com"
-                />
-            </div>
+  return (
+    <div className="flex flex-col items-center justify-start h-full w-full">
+      <div className="flex flex-col w-full">
+        <div className="flex items-center w-full py-3 shadow-md">
+          <a href="/settings">
+            <FaArrowLeft className="ml-8" />
+          </a>
+          <div className="text-[24px] font-rubik font-medium w-full text-center mr-12">
+            Profile
+          </div>
         </div>
-    );
+
+        <UpperProps
+          urlImage="/expert/aubrey.jpg"
+          name={session.data?.user.name}
+          role="Advocate Contributor"
+          reputationScore="2235"
+        />
+        <h2 className="mt-4 mx-10 text-center">
+          With just 27 more Upvotes you can become an Influencer Contributor
+        </h2>
+
+        <hr className="border-t border-gray-300 mx-10 mt-7" />
+
+        <LowerProps
+          fullName={session.data?.user.name}
+          email={session.data?.user.email}
+        />
+      </div>
+    </div>
+  );
 }
