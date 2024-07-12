d0712aa594bb14070e7401b58b88e5753b99d703
shidqizh <shidqizh@gmail.com>
2024-07-12 13:49:32 +0700

    feat: settings page

---
+++ src/app/(site)/(require-login)/(use-navbar)/settings/comp/button.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from 'react';
+
+interface ButtonProps {
+  icon1: React.ReactNode;
+  title: string;
+  subtitle: string;
+  href: string;
+  icon2: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ icon1, title, subtitle, href, icon2 }) => {
+  return (
+    <a
+      href={href}
+      className="flex items-center text-decoration-none p-2.5 mx-1"
+    >
+      <div className="mr-3 rounded-lg bg-violet-800 p-3 flex items-center justify-center">
+        {React.cloneElement(icon1 as React.ReactElement, { className: 'text-white text-xl' })}
+      </div>
+      <div className="flex flex-col flex-1">
+        <div className="font-bold text-lg">
+          {title}
+        </div>
+        <div className="text-sm text-gray-600">
+          {subtitle}
+        </div>
+      </div>
+      <div className="ml-3">
+        {React.cloneElement(icon2 as React.ReactElement, { className: 'text-lg' })}
+      </div>
+    </a>
+  );
+};
+
+export default Button;
